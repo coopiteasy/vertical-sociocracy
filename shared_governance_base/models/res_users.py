fb472632f5413ade382a0af90693ee65ea3251cb
houssine <houssine@DESKTOP-ICALFFT>
2019-01-31 16:45:04 +0100

    [IMP] add related fields from res partner on res users

--- shared_governance_base/models/res_users.py
+++ shared_governance_base/models/res_users.py
@@ -5,3 +5,10 @@ from openerp import api, fields, models
 class ResUsers(models.Model):
 
     _inherit = 'res.users'
+
+    email = fields.Char(
+        related='partner_id.email')
+    phone = fields.Char(
+        related='partner_id.phone')
+    lang = fields.Selection(
+        related='partner_id.lang')
