45a3934e14f98fcf0ea5cb30e70ac9a0946c52c2
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-29 14:19:58 +0200

    [FIX] SCRLfs → SC
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- shared_governance_base/__manifest__.py
+++ shared_governance_base/__manifest__.py
@@ -1,12 +1,12 @@
 #
-#  Copyright (C) 2019  Coop IT Easy SCRLfs. <http://www.coopiteasy.be>
+#  Copyright (C) 2019  Coop IT Easy SC. <http://www.coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Shared Governance",
     "version": "12.0.1.0.0",
     "depends": ["mail"],
-    "author": "Coop IT Easy SCRLfs",
+    "author": "Coop IT Easy SC",
     "category": "Cooperative Management",
     "website": "https://coopiteasy.be",
     "license": "AGPL-3",
